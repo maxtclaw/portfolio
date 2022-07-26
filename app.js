1428de9ce011800bc1a27a4ba4870e6c360b35ff
Max <max.t.c.law@gmail.com>
2022-07-26 17:58:55 -0400

    initial template commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Importing default function from module
+import moduleDefaultFunction from './scripts/script.js'
+
+// Importing named function from module
+import { moduleFunction } from './scripts/script.js'
+
+// Calling imported functions
+moduleDefaultFunction();
+moduleFunction();
+
+
---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// Default export function
+export default function moduleDefaultFunction() {
+    console.log('module default function call')
+}
+
+// Named export function
+export function moduleFunction() {
+    console.log('module function call')
+}
