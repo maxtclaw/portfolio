bf3395ae10bdd05a7e80261645a0fab30345c4f2
Max <max.t.c.law@gmail.com>
2022-08-07 15:19:18 -0400

    added text content, fade in on scroll, images, footer

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 // Manage navbar dropdown menu
 import hamburgerDropdownButtonListener from './scripts/hamburgerDropdown.js';
-import { hamburgerDropdownElementListener, navNameAnchorElementListener } from './scripts/hamburgerDropdown.js'
+import { hamburgerDropdownElementListener, navNameAnchorElementListener } from './scripts/hamburgerDropdown.js';
 
-hamburgerDropdownButtonListener();
-hamburgerDropdownElementListener();
-navNameAnchorElementListener()
+const app = {};
+
+app.init = function () {
+    // Add event listeners for navigation elements
+    hamburgerDropdownButtonListener();
+    hamburgerDropdownElementListener();
+    navNameAnchorElementListener();
+}
+
+app.init()
+
